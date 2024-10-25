58f943b91159325da38d24837542d8dc10647aa4
Tushar Pal <Tusharpal353@gmail.com>
2024-10-25 21:43:30 +0530

    signin and signup route done

--- backend/README.md
+++ backend/README.md
@@ -1,8 +1,30 @@
-```
-npm install
-npm run dev
-```
 
-```
-npm run deploy
-```
+- app.get('/api/v1/blog/:id',(c)=>{
+  return c.text("this is the POST api/v1/signup route")
+})
+-  c here stand for CONTEXT it has req res and all the things
+
+- cloudflare is a serverless architecture means our code is deployed on mini machnies  on the server at multiple server location , our code run on the 100 machines 
+
+- we dont let the server talk directly to our databse we use **CONNECTION POOL** 
+    many server send multiple request to this this send sinlge request to the Database
+
+    - prisma help us in connection pool 
+
+step 1
+- initialize prisma 
+    npm i prisma 
+    npx prisma init
+    change the DB url in the .env file in prisma 
+
+Wrangler.toml
+  it is the place from where enviroment variable will be picked from for the cloudfare worker
+
+Step 2 
+  - create a schema in prisma 
+  - migrate it
+  - generate client - then we can perform FINDONE and all
+  
+
+step 3 
+ - creating a Middleware to protect a end point
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,30 +1,107 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign,verify } from "hono/jwt";
 
-const app = new Hono()
+const app = new Hono();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+app.use('/api/v1/blog/*',async (c,next)=>{
+  //get the header
+  //verify the header
+  //if the header is correct, we need can proceed
+  //if not return 403
+
+  const header=c.req.header("authorization") || "";
+  const tocken = header.split(" ")[1]
+  
+   //@ts-ignore
+  const response = verify(tocken,c.env.JWT_SECRET)
+  if(response.id){
+    next()
+  }else{
+    c.status(403)
+    return c.json({
+      error:"unauthorized"
+    })
+  }
+  await next()
 })
 
 
-app.post('/api/v1/signup',(c)=>{
-  return c.text("this is the POST api/v1/signup route")
+
+
+
+app.get("/", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.post("/api/v1/signup", async (c) => {
+  
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,  
+  }).$extends(withAccelerate());
+
+  const body = await c.req.json();
+
+  // Create the user in the database
+  const user = await prisma.user.create({
+    data: {
+      email: body.email,
+      password: body.password,
+    /*   name:body.name, */
+    }
+  });
+
+  
+  const token = await sign({ id: user.id }, c.env.JWT_SECRET);
+
+  return c.json({ jwt: token });
+
+});
+
+app.post("/api/v1/signin",async  (c) => {
+
+  //  1 initialized the prisma
+  const prisma = new PrismaClient({
+    //@ts-ignore 
+    datasourceUrl: c.env.DATABASE_URL,  
+  }).$extends(withAccelerate());
+
+  // initialized body 
+  const body= await c.req.json()
+const user =await prisma.user.findUnique({
+  where:{
+    email:body.email,
+    password:body.password
+  }
 })
 
-app.post('/api/v1/signin',(c)=>{
-  return c.text("this is the POST api/v1/signin route")
-})
+if(!user){
+  c.status(403)
+  return c.json({
+    error:"user not found "
+  })
+}
 
-app.post('/api/v1/blog',(c)=>{
-  return c.text("this is the POST api/v1/signup route")
-}) 
+const jwt = await sign({id:user.id},c.env.JWT_SECRET)
 
-app.put('/api/v1/blog',(c)=>{
-  return c.text("this is the POST api/v1/signup route")
-})
 
-app.get('/api/v1/blog/:id',(c)=>{
-  return c.text("this is the POST api/v1/signup route")
-})
 
-export default app
+
+  return c.json({jwt});
+});
+/* 
+app.post("/api/v1/blog", (c) => {
+  return c.text("this is the POST api/v1/signup route");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("this is the POST api/v1/signup route");
+});
+
+app.get("/api/v1/blog/:id", (c) => {
+  return c.text("this is the POST api/v1/signup route");
+});
+ */
+export default app;
